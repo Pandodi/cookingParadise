fae2915efde2b728106009a5c7e7a0f9514e5831
Pandodi <68927088+Pandodi@users.noreply.github.com>
2022-04-03 12:09:17 +0200

    initialise project

---
+++ src/test/java/bella/ridah/com/cookingParadise/CookingParadiseApplicationTests.java
@@ -0,0 +1,13 @@
+package bella.ridah.com.cookingParadise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingParadiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
